be91477f1fa5c17d213193277764d69a42245827
John George Chelamattom <tve21cs072@cet.ac.in>
2024-03-03 23:24:07 +0530

    feat: implemeted search

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ async function getLeaderboard() {
 
 export default function App() {
   const [leaderboard, setLeaderboard] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredLeaderboard, setFilteredLeaderboard] = useState([]);
   useEffect(() => {
     getLeaderboard().then((leaderboard) => {
       leaderboard.forEach((lb) => {
@@ -15,8 +17,17 @@ export default function App() {
       });
       const sortedLeaderboard = leaderboard.sort((a, b) => b.total_count - a.total_count);
       setLeaderboard(sortedLeaderboard);
+      setFilteredLeaderboard(sortedLeaderboard);
     });
   }, []);
+
+  useEffect(() => {
+    if (!leaderboard.length) return;
+    const filteredLeaderboard = leaderboard.filter((lb) => {
+      return lb.name.toLowerCase().includes(search.toLowerCase()) || lb.college.toLowerCase().includes(search.toLowerCase());
+    });
+    setFilteredLeaderboard(filteredLeaderboard);
+  }, [search]);
 
   return (
     <>
@@ -25,6 +36,7 @@ export default function App() {
           <h2 className="text-3xl md:text-7xl font-medium text-white">Campus Ambassador</h2>
           <h2 className="text-xl md:text-6xl font-medium text-[#51A1AA]">Leaderboard</h2>
         </div>
+        <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" className="w-5/6 h-12 md:h-20 rounded-lg text-black text-xl md:text-4xl pl-8" placeholder="Search College or Name" />
         <table className="table-auto text-white w-5/6">
           <thead>
             <tr className="text-sm md:text-4xl border-b">
@@ -36,9 +48,9 @@ export default function App() {
           </thead>
           <div className="w-full h-2 md:h-4"></div>
           <tbody className="bg-[#51A1AA17] rounded-lg backdrop-blur-lg" style={{ maxHeight: "300px", overflowY: "auto" }}>
-            {leaderboard.map((lb, index) => (
+            {filteredLeaderboard.map((lb, index) => (
               <tr className="text-xs md:text-2xl text-white" key={index + lb.name}>
-                <td className="text-center py-4">{index + 1}</td>
+                <td className="text-center py-4">{leaderboard.indexOf(lb) + 1}</td>
                 <td className="text-center">{lb.name}</td>
                 <td className="text-center">{lb.college}</td>
                 <td className="text-center">{lb.total_count}</td>
